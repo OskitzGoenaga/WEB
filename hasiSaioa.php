df95c8a8907ed92f05107dcb9b8f067cebe8d04f
Oier Maiza <oie_mai_uga@goierrieskola.org>
2026-01-30 13:10:18 +0100

    Erregistratzeko aukera gehituta.

---
+++ hasiSaioa.php
@@ -0,0 +1,104 @@
+<?php
+session_start();
+require 'konexioa.php'; // tu conexión a la base de datos
+?>
+<!-- Formulario simple -->
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Saioa hasi</title>
+    <link rel="stylesheet" href="orokorra.css" />
+    <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+    <style>
+        body {
+            background-color: #f5f5f7;
+        }
+
+        #testua{
+            color: #444;
+            padding-bottom:30px;
+        }
+        
+        .formularioa {
+            text-align: center;
+            width: 30%;
+            margin: 120px auto;
+            background-color: white;
+            box-shadow: 0 10px 18px rgba(0, 0, 0, 0.30);
+            padding: 50px 0px;
+            border-radius: 20px;
+        }
+        .formularioa>h1{
+            font-size: 40px;
+        }
+
+        .formularioa>form>input {
+            border-radius: 7px;
+            border: 1px solid black;
+            padding: 10px 30px;
+        }
+
+        .formularioa>form>#saioaBtn {
+            margin-top: 10px;
+            background-color: black;
+            color: white;
+            padding: 10px 30px;
+            border-radius: 5px;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="formularioa">
+
+        <h1 >SAIOA HASI</h1><br>
+        <p id="testua">Saskia ikusteko edo arazoak bidaltzeko hasi saioa!</p>
+        <form action="hasiSaioa.php" method="POST">
+
+            <label>Email:</label><br>
+            <input type="email" name="email" required><br><br>
+
+            <label>Pasahitza:</label><br>
+            <input type="password" name="pasahitza" required><br><br>
+
+            <button type="submit" id="saioaBtn">SAIOA HASI</button>
+        </form>
+        <a href="login.php">Ez duzu kontua?</a>
+    </div>
+</body>
+
+</html>
+
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $email = $_POST['email'];
+    $pasahitza = $_POST['pasahitza'];
+
+    // Consulta básica
+    $sql = "SELECT * FROM bezeroak WHERE email = '$email' AND pasahitza = '$pasahitza'";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute();
+    $bezeroa = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    if ($bezeroa) {
+        // Saioan gorde
+        $_SESSION['id'] = $bezeroa['id'];
+        $_SESSION['izena'] = $bezeroa['izena'];
+
+        header("Location: sarrera.php");
+        
+        exit();
+        ?>
+        <script src="https://code.jquery.com/jquery-4.0.0.js" integrity="sha256-9fsHeVnKBvqh3FB2HYu7g2xseAZ5MlN6Kz/qnkASV8U=" crossorigin="anonymous"></script> 
+        <script>alert("Saioa hasi duzu!");</script>
+      <?php
+}
+    } else { ?>
+      <script src="https://code.jquery.com/jquery-4.0.0.js" integrity="sha256-9fsHeVnKBvqh3FB2HYu7g2xseAZ5MlN6Kz/qnkASV8U=" crossorigin="anonymous"></script> 
+      <script>alert("Datu okerrak");</script>
+    <?php }
+?>
--- saskia.php
+++ saskia.php
@@ -3,8 +3,8 @@ include_once "konexioa.php";
 session_start();
 
 
-if (!isset($_SESSION['bezero_id'])) {
-    header("Location: login.php");
+if (!isset($_SESSION['id'])) {
+    header("Location: hasiSaioa.php");
     exit();
 }
 ?>
@@ -42,7 +42,7 @@ if (!isset($_SESSION['bezero_id'])) {
     <?php include_once "navbar.php"; ?>
     <div class="saskia2">
         <h1>Erosketa saskia</h1>
-        <p>Kaixo, <?php echo $_SESSION['bezero_izena']; ?>. Hemen dago zure saskia.</p>
+        <p>Kaixo, <?php echo $_SESSION['izena']; ?>. Hemen dago zure saskia.</p>
     </div>
     
 </body>
