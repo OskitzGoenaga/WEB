338dbb1d6a7c5542483ca0457f9c2764be2ca960
Oier Maiza <oie_mai_uga@goierrieskola.org>
2026-02-06 13:18:52 +0100

    Konexioa igota (arazo batengatik)

--- konexioa.php
+++ konexioa.php
@@ -1,10 +1,10 @@
 <?php
 
 // db.php
-$host = "localhost"; //
+$host = "192.168.115.163"; //
 $dbname = "db_erronka2";
-$user = "root"; // XAMPP: root
-$pass = "1MG32025"; // XAMPP: sarri hutsik
+$user = "kudeatzailea"; // XAMPP: root
+$pass = "1MG3_2025"; // XAMPP: sarri hutsik
 
 $dsn = "mysql:host=$host;dbname=$dbname;charset=utf8mb4";
 
